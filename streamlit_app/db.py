abe46241f8c3b791110a9e820247c015ccad8a65
ShubhT <Shubhcodes1625@gmail.com>
2026-09-23 19:57:17 +0530

    Add Streamlit Cloud secrets support

--- streamlit_app/db.py
+++ streamlit_app/db.py
@@ -2,16 +2,26 @@ import os
 from sqlalchemy import create_engine
 from dotenv import load_dotenv
 from urllib.parse import quote_plus
+import streamlit as st  
 
 load_dotenv()
 
 
 def get_engine():
-    host = os.getenv("DB_HOST")
-    port = os.getenv("DB_PORT")
-    database = os.getenv("DB_NAME")
-    user = os.getenv("DB_USER")
-    password = quote_plus(os.getenv("DB_PASSWORD"))
+    try:
+        host = st.secrets["DB_HOST"]
+        port = st.secrets["DB_PORT"]
+        database = st.secrets["DB_NAME"]
+        user = st.secrets["DB_USER"]
+        password = st.secrets["DB_PASSWORD"]
+    except st.errors.StreamlitSecretNotFoundError:
+        host = os.getenv("DB_HOST")
+        port = os.getenv("DB_PORT")
+        database = os.getenv("DB_NAME")
+        user = os.getenv("DB_USER")
+        password = os.getenv("DB_PASSWORD")
+
+    password = quote_plus(password)
 
     database_url = (
         f"postgresql+psycopg2://{user}:{password}"
